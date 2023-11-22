3e0612a7d51bc7816aee20a3e6d3cd16a7e62c7f
syedmehdi-bst <syedmehdi.abbas@bluestacks.com>
2023-11-22 10:38:39 +0530

    separte edit video function

--- backend/app.py
+++ backend/app.py
@@ -50,7 +50,8 @@ def upload():
             ffmpeg.run(stream)
             logging.info("clip written to temp file")
             temp_file.seek(0)
-            upload_res, new_video_id = upload_video(temp_file.name, title, auth_token)
+            upload_url, new_video_id = create_video(title, auth_token)
+            upload_res = upload_video(temp_file.name, title, upload_url)
             temp_file.close()
             os.remove(temp_file.name)
         
@@ -73,6 +74,69 @@ def upload():
         return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
 
 
+def edit_video(video_id, title, trim, crop, auth_token, input_video_url, upload_url):
+    request_init_time = time.time()
+
+    stream = ffmpeg.input(input_video_url)
+
+    if trim:
+        trim_start = int(float(trim.get("start", "0")))
+        trim_end = int(float(trim.get("end", "1")))
+        # validate input
+        stream = ffmpeg.trim(stream, start=trim_start, end=trim_end)
+
+    if crop:
+        x1 = int(float(crop["x1"]))
+        y1 = int(float(crop["y1"]))
+        x2 = int(float(crop["x2"]))
+        y2 = int(float(crop["y2"]))
+        width = x2 - x1
+        height = y2 - y1
+        # validate input
+        stream = ffmpeg.crop(stream, x1, y1, width, height)
+
+    # write clip to a temp file
+    with tempfile.NamedTemporaryFile(suffix=".mp4") as temp_file:
+        log_resource_usage(f'temp file created {temp_file.name}')
+        try:
+            stream = ffmpeg.filter(stream, 'scale', 1280, -1)
+            stream = ffmpeg.output(stream, temp_file.name)
+            stream = ffmpeg.overwrite_output(stream)
+            ffmpeg.run(stream)
+        except ffmpeg.Error as e:
+            logging.error(e.stderr)
+            return jsonify({"status": "error", "message": f'Something went wrong while writing the video', "error": str(e)}), 500
+        except Exception as e:
+            logging.error(e)
+            log_resource_usage()
+            return jsonify({"status": "error", "message": f'Something went wrong while writing the video', "error": str(e)}), 500
+        log_resource_usage("clip written to temp file")
+        temp_file.seek(0)
+        upload_res = upload_video(temp_file.name, title, upload_url)
+        temp_file.close()
+        del temp_file
+    
+    log_resource_usage()
+
+    if upload_res.status_code != 200:
+        return jsonify({"status": "error", "message": "Something went wrong while uploading the video"}), upload_res.status_code
+
+    delete_res = delete_video(video_id, auth_token)
+    if delete_res.status_code != 200:
+        return jsonify({"status": "error", "message": "Something went wrong while deleting the previous video"}), delete_res.status_code
+    logging.info(f'previous video deleted: {video_id}')
+
+    res_dict = {
+        "status": "success",
+        "message": "Video processed successfully",
+        "request_processing_time": time.time() - request_init_time
+    }
+
+    logging.info(f'response to be sent: {res_dict}')
+    log_resource_usage()
+    return jsonify(res_dict), 200
+
+
 @app.route("/video/process", methods=["POST"])
 def process():
     try:
@@ -84,81 +148,23 @@ def process():
         auth_token = request.headers.get("token")
 
         logging.info(f'video edit request for {video_id}')
-        request_init_time = time.time()
-        log_resource_usage("request hit")
 
         try:
             video_info = get_video_info(video_id)
+            if not video_info:
+                return jsonify({"status": "error", "message": f'Info for video with id {video_id} not found'}), 404
             video_url = video_info["downloadUrl"]
-            logging.info("video info received")
+            if not video_url:
+                return jsonify({"status": "error", "message": f'Download url for video with id {video_id} not found'}), 404
+            logging.info("downloadUrl received")
         except Exception as e:
-            return jsonify({"status": "error", "message": f'Something went wrong while getting downloadUrl for {video_id}', "error": str(e)}), 500
-
-        if not video_url:
-            return jsonify({"status": "error", "message": f'Download url for video with id {video_id} not found'}), 404
-
-        stream = ffmpeg.input(video_url)
-
-        if trim:
-            trim_start = int(float(trim.get("start", "0")))
-            trim_end = int(float(trim.get("end", "1")))
-            # validate input
-            stream = ffmpeg.trim(stream, start=trim_start, end=trim_end)
-
-        if crop:
-            x1 = int(float(crop["x1"]))
-            y1 = int(float(crop["y1"]))
-            x2 = int(float(crop["x2"]))
-            y2 = int(float(crop["y2"]))
-            width = x2 - x1
-            height = y2 - y1
-            # validate input
-            stream = ffmpeg.crop(stream, x1, y1, width, height)
-
-        log_resource_usage()
-
-        # write clip to a temp file
-        with tempfile.NamedTemporaryFile(suffix=".mp4") as temp_file:
-            log_resource_usage(f'temp file created {temp_file.name}')
-            try:
-                stream = ffmpeg.filter(stream, 'scale', 1280, -1)
-                stream = ffmpeg.output(stream, temp_file.name)
-                stream = ffmpeg.overwrite_output(stream)
-                ffmpeg.run(stream)
-            except ffmpeg.Error as e:
-                logging.error(e.stderr)
-                return jsonify({"status": "error", "message": f'Something went wrong while writing the video', "error": str(e)}), 500
-            except Exception as e:
-                logging.error(e)
-                log_resource_usage()
-                return jsonify({"status": "error", "message": f'Something went wrong while writing the video', "error": str(e)}), 500
-            log_resource_usage("clip written to temp file")
-            temp_file.seek(0)
-            upload_res, new_video_id = upload_video(temp_file.name, title, auth_token)
-            temp_file.close()
-            del temp_file
+            return jsonify({"status": "error", "message": f'Something went wrong with getting downloadUrl of {video_id}', "error": str(e)}), 500
         
-        logging.info(f'upload done for new video {new_video_id}')
-        log_resource_usage()
+        upload_url, new_video_id = create_video(title, auth_token)
+        logging.info(f'new video id: {new_video_id}')
 
-        if upload_res.status_code != 200:
-            return jsonify({"status": "error", "message": "Something went wrong while uploading the video"}), upload_res.status_code
-
-        delete_res = delete_video(video_id, auth_token)
-        if delete_res.status_code != 200:
-            return jsonify({"status": "error", "message": "Something went wrong while deleting the previous video"}), delete_res.status_code
-        logging.info(f'previous video deleted: {video_id}')
-
-        res_dict = {
-            "status": "success",
-            "message": "Video processed successfully",
-            "video_id": new_video_id,
-            "request_processing_time": time.time() - request_init_time
-        }
-
-        logging.info(f'response to be sent: {res_dict}')
-        log_resource_usage()
-        return jsonify(res_dict), 200
+        res = edit_video(video_id, title, trim, crop, auth_token, video_url, upload_url)
+        return res
 
     except Exception as e:
         logging.error(e)
@@ -277,12 +283,11 @@ def pull_messages():
         logging.error(e)
 
 
-def upload_video(video_path, title, token):
+def upload_video(video_path, title, cloudflare_upload_url):
     try:
-        cloudflare_upload_url, new_video_id = create_video(title ,token)
         with open(video_path, 'rb') as video_file:
             res = requests.post(cloudflare_upload_url, files={"file": (title, video_file)})
-        return res, new_video_id
+        return res
     except Exception as e:
         logging.error(e)
 
@@ -292,6 +297,7 @@ def create_video(title, token):
         headers = {
             'Authorization': f'Bearer {token}'
         }
+        # TODO: send data from video info
         create_video_body = {
             "title": title,
             "description": "",
