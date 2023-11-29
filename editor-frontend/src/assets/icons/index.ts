982ea68f92a721f14b2445c9d4f8f35173a53f5c
syedmehdi-bst <syedmehdi.abbas@bluestacks.com>
2023-11-29 13:23:25 +0530

    icons

--- editor-frontend/src/assets/icons/index.ts
+++ editor-frontend/src/assets/icons/index.ts
@@ -4,3 +4,5 @@ export { IconDownload } from "./IconDownload";
 export { IconEdit } from "./IconEdit";
 export { IconProductLogo } from "./IconProductLogo";
 export { IconTrash } from "./IconTrash";
+export { IconCrop } from "./IconCrop";
+export { IconTrim } from "./IconTrim";
