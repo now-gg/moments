c5e4733e28630146db6f69e56c4be2a7ff026539
syedmehdi-bst <syedmehdi.abbas@bluestacks.com>
2023-12-12 16:29:03 +0530

    send stats through cloud api

--- backend/bigquery.py
+++ backend/bigquery.py
@@ -2,9 +2,8 @@ from google.cloud import bigquery
 import logging
 from datetime import datetime
 from enum import Enum
-from constants import BQ_DATASET_NAME, BQ_TABLE_NAME, GCP_PROJECT_ID
-
-client = bigquery.Client(project=GCP_PROJECT_ID)
+from constants import BLUESTACKS_CLOUD_HOST
+import requests
 
 # Event Types
 VIDEO_EDIT_REQUEST = "video_edit_request"
@@ -12,24 +11,23 @@ VIDEO_EDIT_PROCESSED =  "video_edit_processed"
 VIDEO_DELETED =  "video_deleted"
 
 
-def insert_data(data):
-    try:
-        dataset_ref = client.dataset(BQ_DATASET_NAME)
-        table_ref = dataset_ref.table(BQ_TABLE_NAME)
-        table = client.get_table(table_ref)
-        errors = client.insert_rows(table, [data])
-        if errors == []:
-            logging.info("Data added to BQ")
-        else:
-            logging.info("Error while adding data to BQ: {}".format(errors))
-    except Exception as e:
-        logging.error(e)
-
-
 def send_stat_to_bq(event, data):
     try:
-        data["event_type"] = event
-        data["created_at"] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-        insert_data(data)
+        stats_url = f'{BLUESTACKS_CLOUD_HOST}/app_player/miscellaneousstats'
+        form_data = {
+            "event_type": event,
+            "created_at": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+            "tag": "moments",
+            "arg1": data.get("arg1", ""),
+            "arg2": data.get("arg2", ""),
+            "arg3": data.get("arg3", ""),
+            "arg4": data.get("arg4", ""),
+            "arg5": data.get("arg5", ""),
+        }
+        res = requests.post(stats_url, data=form_data)
+        if res.status_code == 200:
+            logging.info("Stats sent to BQ")
+        else:
+            logging.error("Error while sending stats to BQ: {}".format(res.text))
     except Exception as e:
         logging.error(e)
