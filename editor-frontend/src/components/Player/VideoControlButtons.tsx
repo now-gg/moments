08e4a673e6bad492cd940b5cdc696ad9e30fcaeb
syedmehdi-bst <syedmehdi.abbas@bluestacks.com>
2023-12-12 15:19:48 +0530

    aspectRatio sent  to process call

--- editor-frontend/src/components/Player/VideoControlButtons.tsx
+++ editor-frontend/src/components/Player/VideoControlButtons.tsx
@@ -266,6 +266,7 @@ const VideoControlButtons = ({ videoUrl, startTime, endTime, setStartTime, setEn
 
     if(payload["trim"] || payload["crop"]) {
       payload["title"] = title === videoInfo.title ? title + " (copy)" : title;
+      payload["aspectRatio"] = payload["crop"] ? aspectRatio : "";
       const loadingToast = toast.loading("Editing your video, please wait");
       setShowEditingOverlay(true);
       fetch(`${import.meta.env.VITE_BACKEND_HOST}/video/process`, {
