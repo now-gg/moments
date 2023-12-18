93d2e6677ebe434122ebc4bfc67b5701bfaf92aa
syedmehdi-bst <syedmehdi.abbas@bluestacks.com>
2023-12-18 13:13:20 +0530

    add send_response and security headers

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 
 import google.cloud.logging
 import logging
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, send_from_directory
 import json
 import os
 import time
@@ -15,9 +15,10 @@ from redis_wrapper  import RedisWrapper
 from bigquery import send_stat_to_bq, VIDEO_EDIT_PROCESSED, VIDEO_EDIT_REQUEST, VIDEO_DELETED
 from uuid import uuid4
 from constants import VIDEO_PORTAL_HOST, ALLOWED_ORIGINS
+from utils import send_response
 
 app = Flask(__name__)
-CORS(app, origins=ALLOWED_ORIGINS)
+CORS(app, origins=ALLOWED_ORIGINS, methods=['GET','POST'])
 
 client = google.cloud.logging.Client()
 client.setup_logging()
@@ -54,18 +55,18 @@ def process():
 
         video_cache_key = f'moments-editor-video-{video_id}'
         if redis_client.get(video_cache_key) == "processing":
-            return jsonify({"status": "error", "message": f'Video with id {video_id} is already being processed'}), 400
+            return send_response({"message": f'Video with id {video_id} is already being processed'}, 400)
 
         try:
             video_info = get_video_info(video_id)
             if not video_info:
-                return jsonify({"status": "error", "message": f'Info for video with id {video_id} not found'}), 404
+                return send_response({"message": f'Info for video with id {video_id} not found'}, 404)
             video_url = video_info["downloadUrl"]
             if not video_url:
-                return jsonify({"status": "error", "message": f'Download url for video with id {video_id} not found'}), 404
+                return send_response({"message": f'Download url for video with id {video_id} not found'}, 404)
             logging.info("downloadUrl received")
         except Exception as e:
-            return jsonify({"status": "error", "message": f'Something went wrong with getting downloadUrl of {video_id}', "error": str(e)}), 500
+            return send_response({"message": f'Something went wrong with getting downloadUrl of {video_id}', "error": str(e)}, 500)
 
         create_video_res = create_video(title, auth_token, video_info)
         if create_video_res.status_code != 200:
@@ -102,13 +103,13 @@ def process():
         logging.info(f'message to be published: {message}')
         redis_client.set(video_cache_key, "processing")
         publish_message(json.dumps(message))
-        return jsonify({"status": "success", "message": "Video processing started", "new_video_id": new_video_id}), 200
+        return send_response({"message": "Video processing started", "new_video_id": new_video_id}, 200)
 
     except Exception as e:
         logging.error(e)
         if isinstance(e, KeyError):
-            return jsonify({"status": "error", "message": f'Key {e} missing from request body'}), 400
-        return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
+            return send_response({ "message": f'Key {e} missing from request body'}, 400)
+        return send_response({ "message": f'Something went wrong', "error": str(e)}, 500)
 
 
 @app.route("/video/title", methods=["POST"])
@@ -131,7 +132,7 @@ def edit_title():
         return res.json(), res.status_code
     except Exception as e:
         logging.error(e)
-        return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
+        return send_response({ "message": f'Something went wrong', "error": str(e)}, 500)
 
 
 @app.route("/video/delete", methods=["POST"])
@@ -145,18 +146,15 @@ def delete():
 
         delete_res = delete_video(video_id, auth_token)
         if delete_res.status_code != 200:
-            return jsonify({"status": "error", "message": "Something went wrong while deleting the video"}), delete_res.status_code
+            return send_response({ "message": "Something went wrong while deleting the video"}, delete_res.status_code)
         send_stat_to_bq(VIDEO_DELETED, {"arg1": video_id})
-        return jsonify({
-            "status": "success",
-            "message": "Video deleted successfully",
-        }), 200
+        return send_response({"message": "Video deleted successfully",}, 200)
 
     except Exception as e:
         logging.error(e)
         if isinstance(e, KeyError):
-            return jsonify({"status": "error", "message": f'Key {e} missing from request body'}), 400
-        return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
+            return send_response({ "message": f'Key {e} missing from request body'}, 400)
+        return send_response({ "message": f'Something went wrong', "error": str(e)}, 500)
 
 
 @app.route("/video/info", methods=["GET"])
@@ -172,17 +170,13 @@ def info():
 
         if video_info:
             logging.info("video info received", video_info)
-            return jsonify({
-                "status": "success",
-                "message": "Video info received successfully",
-                "video": video_info
-            }), 200
+            return send_response({"message": "Video info received successfully", "video": video_info}, 200)
         
-        return jsonify({"status": "error", "message": f'Video with id {video_id} not found'}), 404
+        return send_response({ "message": f'Video with id {video_id} not found'}, 404)
 
     except Exception as e:
         logging.error(e)
-        return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
+        return send_response({ "message": f'Something went wrong', "error": str(e)}, 500)
 
 
 @app.route("/video/status", methods=["POST"])
@@ -204,13 +198,10 @@ def status():
             res_status = "processing"            
         elif video_status == "failed":
             res_status = "failed"
-        return jsonify({
-            "status": res_status,
-            "message": "Video status received successfully",
-        }), 200
+        return send_response({"message": "Video status received successfully"}, 200)
     except Exception as e:
         logging.error(e)
-        return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
+        return send_response({ "message": f'Something went wrong', "error": str(e)}, 500)
 
 
 def edit_video(request_id, video_id, title, trim, crop, auth_token, input_video_url, upload_url, new_video_id):
@@ -261,13 +252,13 @@ def edit_video(request_id, video_id, title, trim, crop, auth_token, input_video_
                 data_for_bq["arg5"] = json.dumps(time_log)
                 send_stat_to_bq(VIDEO_EDIT_PROCESSED, data_for_bq)
                 logging.error(e.stderr)
-                return jsonify({"status": "error", "message": f'Something went wrong while writing the video', "error": str(e)}), 500
+                return send_response({ "message": f'Something went wrong while writing the video', "error": str(e)}, 500)
             except Exception as e:
                 redis_client.set(video_cache_key, "failed", xx=True)
                 data_for_bq["arg5"] = json.dumps(time_log)
                 send_stat_to_bq(VIDEO_EDIT_PROCESSED, data_for_bq)
                 logging.error(e)
-                return jsonify({"status": "error", "message": f'Something went wrong while writing the video', "error": str(e)}), 500
+                return send_response({ "message": f'Something went wrong while writing the video', "error": str(e)}, 500)
             logging.info("clip written to temp file")
             temp_file.seek(0)
             upload_res = upload_video(temp_file.name, title, upload_url)
@@ -281,7 +272,7 @@ def edit_video(request_id, video_id, title, trim, crop, auth_token, input_video_
             data_for_bq["arg5"] = json.dumps(time_log)
             send_stat_to_bq(VIDEO_EDIT_PROCESSED, data_for_bq)
             logging.error("error while uploading video", upload_res.json())
-            return jsonify({"status": "error", "message": "Something went wrong while uploading the video"}), upload_res.status_code
+            return send_response({ "message": "Something went wrong while uploading the video"}, upload_res.status_code)
 
 
         # delete_res = delete_video(video_id, auth_token)
@@ -291,7 +282,7 @@ def edit_video(request_id, video_id, title, trim, crop, auth_token, input_video_
         #     data_for_bq["arg5"] = json.dumps(time_log)
         #     send_stat_to_bq(VIDEO_EDIT_PROCESSED, data_for_bq)
         #     logging.error("error while deleting old video", delete_res.json())
-        #     return jsonify({"status": "error", "message": "Something went wrong while deleting the previous video"}), delete_res.status_code
+        #     return send_response({ "message": "Something went wrong while deleting the previous video"}, delete_res.status_code)
 
         time_log["total"] = time.time() - request_init_time
         data_for_bq["arg4"] = "success"
@@ -299,19 +290,18 @@ def edit_video(request_id, video_id, title, trim, crop, auth_token, input_video_
         send_stat_to_bq(VIDEO_EDIT_PROCESSED, data_for_bq)
 
         res_dict = {
-            "status": "success",
             "message": "Video processed successfully"
         }
 
         redis_client.set(video_cache_key, "success", xx=True)
 
         logging.info(f'response to be sent: {res_dict}')
-        return jsonify(res_dict), 200
+        return send_response(res_dict, 200)
     
     except Exception as e:
         redis_client.set(video_cache_key, "failed", xx=True)
         logging.error(e)
-        return jsonify({"status": "error", "message": f'Something went wrong', "error": str(e)}), 500
+        return send_response({ "message": f'Something went wrong', "error": str(e)}, 500)
 
 
 def pull_message_callback(message):
